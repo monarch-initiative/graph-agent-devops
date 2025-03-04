08ef108f5ae1979a6e375a7ee25bd4d00eb3e473
Seth Carbon <github.com@genkisugi.net>
2025-03-03 18:08:48 -0800

    try and override sg

--- provision/aws/main.tf
+++ provision/aws/main.tf
@@ -70,26 +70,3 @@ output "dns_records" {
 output "public_ip" {
    value                  = module.base.public_ip
 }
-
-resource "aws_security_group" "sg-c2463bde" {
-  vpc_id = var.vpc_id
-  tags   = var.tags
-
-  dynamic "ingress" {
-      for_each = var.open_ports
-
-      content {
-        from_port   = ingress.value
-        to_port     = ingress.value
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-      }
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
